e9167eb9aa200482de0ea2b2435e8b3e25f042f2
Aditya singh <157194334+adi-singh123@users.noreply.github.com>
2026-02-19 15:02:35 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,4 @@ import { meta } from 'eslint-plugin-react-hooks'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: meta.env.VITE_BASE_PATH || "/attendance-task",
 })
