5910c4402f9013800ab5eaae70263363efaf1501
dev-singh123 <adityasingh@gmail.com>
2026-02-19 14:46:35 +0530

    vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { meta } from 'eslint-plugin-react-hooks'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: meta.env.VITE_BASE_PATH || "/attendance-task",
 })
